b7cd557ff1cb04f64094c23ef0aed5a6d689492c
blueni <blueni@zuwoojia.com>
2017-09-21 11:08:29 +0800

    返回客户端一个文件

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 
 const http = require( 'http' )
+const path = require( 'path' )
+const { readFile } = require( './read-file.js' )
+
 const PORT = 8090
 
 http.createServer( ( req, res ) => {
-    res.end( 'hello world!' )
+    let filename = path.join( __dirname, 'static/index.html' )
+    readFile( filename, ( err, content ) => {
+        res.end( content )
+    })
 }).listen( PORT )
 
 console.log( `server run at port ${PORT}` )
--- read-file.js
+++ read-file.js
@@ -1,8 +1,5 @@
 const fs = require( 'fs' )
-const path = require( 'path' )
 
-let filename = path.join( __dirname, 'index.js' )
-
-fs.readFile( filename, 'utf-8', ( err, content ) => {
-    console.log( content )
-})
+exports.readFile = ( filename, cb = () => 0 ) => {
+    fs.readFile( filename, 'utf-8', cb )
+}
